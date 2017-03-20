08ecbb71953a1e414ca9feb576b3806321541660
kkkkoyo <mmkrz.sg@gmail.com>
2017-03-20 12:56:09 +0900

    add DataManual

---
+++ Assets/Scripts/Data/DataManual.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+	kkkkoyo original sample
+*/
+public class DataManual : MonoBehaviour
+{
+    /*パネルをスライド */
+    [SerializeField] private PanelSlider panel;
+    public void PanelSlideIn()
+    {
+        panel.SlideIn();
+    }
+    public void PanelSlideOut()
+    {
+        panel.SlideOut();
+    }
+
+    /*シーンの遷移*/
+    public void GoNextScene(bool isAsynchronous, string SceneName)
+    {
+        if (isAsynchronous)
+        {
+            StartCoroutine(GoGame(SceneName));
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+    private IEnumerator GoGame(string SceneName)
+    {
+        AsyncOperation async;
+        async = SceneManager.LoadSceneAsync(SceneName);
+        yield return async;
+    }
+}
