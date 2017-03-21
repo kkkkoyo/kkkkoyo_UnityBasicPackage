d6e0f69d4f860944e1ad596a3f4fcfb0aab82601
kkkkoyo <mmkrz.sg@gmail.com>
2017-03-21 11:32:01 +0900

    add  often use method

--- Assets/Scripts/Data/DataManual.cs
+++ Assets/Scripts/Data/DataManual.cs
@@ -10,6 +10,7 @@ public class DataManual : MonoBehaviour
 {
     /*パネルをスライド */
     [SerializeField] private PanelSlider panel;
+
     public void PanelSlideIn()
     {
         panel.SlideIn();
@@ -36,5 +37,52 @@ public class DataManual : MonoBehaviour
         AsyncOperation async;
         async = SceneManager.LoadSceneAsync(SceneName);
         yield return async;
+    }
+    private void ListPrac()
+    {
+        List<int> intList = new List<int>();    //int型のリスト
+        intList.Add(1); // intList = {1}
+        intList.Add(2); // intList = {1,2}
+        intList.Add(3); // intList = {1,2,3}
+        intList.RemoveAt(1);    // intList = {1,3}
+        int b = intList[1]; // bには3が入る
+        if(intList.Contains(1))
+        {
+            Debug.Log("1がある");
+        }
+    }
+
+    //接触したときのイベント用メソッド 
+    void OnTriggerEnter(Collider collider)
+    {
+    
+    }
+    //接触している間、常に発生し続けるイベント用メソッド 
+    void OnTriggerStay(Collider collider) 
+    {
+
+    }
+    //離れたときのイベント用メソッド 
+    void OnTriggerExit(Collider collider) 
+    {
+
+    }
+
+    //オブジェクトが衝突したとき
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    
+    //オブジェクトが離れた時
+    void OnCollisionExit(Collision collision)
+    {
+
+    }
+    
+    //オブジェクトが触れている間
+    void OnCollisionStay(Collision collision)
+    {
+
     }
 }
