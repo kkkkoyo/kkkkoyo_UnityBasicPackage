4298f7ee36718e98d82873962b5804c09a6dbf5a
kkkkoyo <mmkrz.sg@gmail.com>
2017-12-11 16:27:45 +0900

    added panel control

---
+++ Assets/Scripts/Data/PanelControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelControl : MonoBehaviour {
+
+	public PanelSlider[] panel;
+
+
+	public void PanelIn(int num)
+	{
+		panel[num].SlideIn();
+	}
+	public void PanelOut(int num)
+	{
+		panel[num].SlideOut();
+	}
+}
