bf81d4676258ed207a7ec7d9b3eed9ca9f6da898
kkkkoyo <mmkrz.sg@gmail.com>
2018-04-08 18:33:11 +0900

    jsonのつかいかた　追加

---
+++ Assets/Scripts/Data/JsonUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class users {
+    public Itemquestions[] questions;
+}
+
+[Serializable]
+public class Itemquestions
+{
+    public string name;
+    public int age;
+}
+public class JsonUtil : MonoBehaviour {
+
+    public Text txtString;
+
+    void Start() {
+        TextAsset txtData = Resources.Load("Json/test1")as TextAsset;
+        string textJson = txtData.text;
+
+        users item = JsonUtility.FromJson<users>(textJson);
+        Debug.Log(item.questions[0].age+":"+item.questions[0].name);
+
+    }
+}
